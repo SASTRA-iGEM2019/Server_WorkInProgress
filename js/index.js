70c209fdc8a4e5a609177c47b564d0e7d86da241
Vathsan <srivaths311@gmail.com>
2019-08-27 12:29:42 +0530

    Support for hosting

--- js/index.js
+++ js/index.js
@@ -1,27 +1,33 @@
 function change() {
-    var object = document.getElementById("error");
-    object.style.display = "none";
+    document.getElementById("error").style.display = "none";
+
     var complementary_bases = { A:"U", C:"G", G:"C", U:"A"};
     var stop_codons = ["UAG","UAA","UGA"];
-
     var rna = document.getElementById('targetrna').value;
-    trna = rna.match(/.{1,3}/g);
-    for(i=0;i<trna.length;i++){
-        if(trna[i]=="UAG" ||trna[i]=="UAA" ||trna[i]=="UGA"){
-            var obj = document.getElementById("error");
-            obj.style.display = "block";
+
+    if(rna != ""){
+        trna = rna.match(/.{1,3}/g);
+        for(i=0;i<trna.length;i++){
+            if(trna[i]=="UAG" ||trna[i]=="UAA" ||trna[i]=="UGA"){
+                var obj = document.getElementById("error");
+                obj.style.display = "block";
+            }
         }
+        console.log(trna)
+        rna = rna.toUpperCase().replace(/[^ACGU]/g,"")
+        var revrna = rna.split("").reverse().join("");
+        var b = rna.split("");
+        var comprna="";
+        for(i=0;i<b.length;i++){
+            comprna += complementary_bases[b[i]]
+        }
+        comprev = comprna.split("").reverse().join("");
+        document.getElementById("rev").innerHTML = revrna;
+        document.getElementById("comp").innerHTML = comprna;
+        document.getElementById("revcomp").innerHTML = comprev;
+    }else{
+        document.getElementById("rev").innerHTML = "";
+        document.getElementById("comp").innerHTML = "";
+        document.getElementById("revcomp").innerHTML = "";
     }
-    console.log(trna)
-    rna = rna.toUpperCase().replace(/[^ACGU]/g,"")
-    var revrna = rna.split("").reverse().join("");
-    var b = rna.split("");
-    var comprna="";
-    for(i=0;i<b.length;i++){
-        comprna += complementary_bases[b[i]]
-    }
-    comprev = comprna.split("").reverse().join("");
-    document.getElementById("rev").innerHTML = revrna;
-    document.getElementById("comp").innerHTML = comprna;
-    document.getElementById("revcomp").innerHTML = comprev;
 }
